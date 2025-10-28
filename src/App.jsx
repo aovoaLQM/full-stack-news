6344d0cd5ed8c613cdebe39c7ae7c7e0851d1438
aovoaLQM <leduc2546@gmail.com>
2025-10-28 17:25:42 +0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import Navbar from "./components/Navbar"
+
 const App = () => {
   return (
-    <div className=''>Hello World</div>
+    <div className='px-4 md:px8- lg:px-16 xl:px-32 2xl:px-64'  >
+    { /* Navvar*/}
+    <Navbar />
+    { /* breadcrumb*/}
+    { /*introduction*/}
+    { /* featured news*/}
+    { /* new list*/}
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react"
+
+const Navbar = () => {
+const [open,setOpen] = useState(false) ;
+    return (
+        <div className="w-full h-16 md:h-20 flex items-center justify-between "> 
+            { /* Logo*/}
+            <div className="flex items-center gap-4 text-2xl font-bold">
+             <img src="/logo.png" className="w-8 h-8" alt="logo" />
+             <span> lamalog</span>
+            </div>
+            {/* mobile menu*/}
+            <div className=" md:hidden ">
+                {/* mobile button*/}
+                <div className="cursor-pointer text-4xl" 
+                onClick={() => setOpen((prev) => !prev)}
+                >
+                    {open ? "X" : "≡"}
+                </div>
+                {/* mobile link list */}
+                <div className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${
+                    open ? "-right-0" : " -right-[100%]"
+                }` }>
+                <a href="/"> Home</a>
+                <a href="/"> Category </a>
+                <a href="/"> Trending</a>
+                <a href="/"> About us</a>
+                <a href="/"> 
+                    <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login</button>
+                </a>
+
+                </div>
+            </div>
+            {/* desktop menu*/}
+            <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+                <a href="/"> Home</a>
+                <a href="/"> Category </a>
+                <a href="/"> Trending</a>
+                <a href="/"> About us</a>
+                <a href="/"> 
+                    <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login</button>
+                </a>
+            </div>
+         </div>
+
+    )
+}
+
+export default Navbar
